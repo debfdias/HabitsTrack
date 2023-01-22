44879075286a36ed391c7afaebf3eea182e3c345
debfdias <debfdias@gmail.com>
2023-01-22 16:32:06 -0300

    Summary table frontend

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx", "./index.html"],
+  content: ["./src/**/*.tsx", "./index.html", "./App.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-};
+  theme: {
+    extend: {
+      colors: {
+        background: "#09090a"
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))"
+      }
+    }
+  },
+  plugins: []
+}
