398dc7d6af59b188e2ccefdd0db763b8071fcdcc
debfdias <debfdias@gmail.com>
2023-01-24 10:59:28 -0300

    Add pop up and pop over components

--- web/src/components/SummaryTable/index.tsx
+++ web/src/components/SummaryTable/index.tsx
@@ -1,12 +1,12 @@
-import { generateDatesFromYearBeginning } from "../../utils/generateDatesFromYearBeginning"
-import { HabitDay } from "../Habit"
+import { generateDatesFromYearBeginning } from "../../utils/generateDatesFromYearBeginning";
+import { HabitDay } from "../Habit";
 
-const weekDays = ["S", "M", "T", "W", "T", "F", "S"]
+const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
 
-const summaryDates = generateDatesFromYearBeginning()
+const summaryDates = generateDatesFromYearBeginning();
 
-const minimumSummaryDatesSize = 18 * 7 // 18 weeks
-const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -20,13 +20,19 @@ export function SummaryTable() {
             >
               {weekDay}
             </div>
-          )
+          );
         })}
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
@@ -36,9 +42,9 @@ export function SummaryTable() {
                 key={i}
                 className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
               />
-            )
+            );
           })}
       </div>
     </div>
-  )
+  );
 }
